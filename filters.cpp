5a33933f13b7048e8bad60d46a989b6e7a6555cd
Ahmed Taha <bjabotaha@yahoo.com>
2022-04-07 16:24:47 +0200

    7/4

--- filters.cpp
+++ filters.cpp
@@ -35,63 +35,6 @@ void saveImage () {
     writeGSBMP(imageFileName, image);
 }
 
-void Merge() {
-
-    char imageFileName2[100];
-
-    cout<<"Enter the source image file name: ";
-    cin >> imageFileName2;
-    strcat (imageFileName2, ".bmp");
-    readGSBMP(imageFileName2, image2);
-
-    for (int i=0; i<SIZE; i++) {
-        for (int j=0; j<SIZE; j++) {
-            image[i][j] = (image[i][j] + image2[i][j])/2;
-        }
-    }
-}
-
-void Shrink() {
-
-    int choice, min;
-    cout<<"1-to shrink image dimension to 1/2:\n2-to shrink image dimension to 1/3:\n3-to shrink image dimension to 1/4:\n";
-    cin >> choice;
-
-    switch (choice) {
-
-        case 1:
-            min = 2;
-            break;
-
-        case 2:
-            min = 3;
-            break;
-
-        case 3:
-            min = 4;
-            break;
-
-        default :
-            cout << "invalid input .";
-            return;
-    }
-
-    int row = 0;
-
-    for (int i = 0; i < SIZE; i++) {
-        int col = 0;
-        for (int j = 0; j < SIZE; j++) {
-            if (i > SIZE / min || j > SIZE / min) {
-                image[i][j] = 255;
-
-            } else
-                image[i][j] = image[row][col];
-            col += min;
-        }
-        row += min;
-    }
-}
-
 void f1_black_white() {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j< SIZE; j++) {
@@ -114,13 +57,44 @@ void f2_invert() {
     }
 }
 
-void f5_rotate() {
+void f3_Merge() {
 
+    char imageFileName2[100];
+
+    cout<<"Enter the source image file name: ";
+    cin >> imageFileName2;
+    strcat (imageFileName2, ".bmp");
+    readGSBMP(imageFileName2, image2);
+
+    for (int i=0; i<SIZE; i++) {
+        for (int j=0; j<SIZE; j++) {
+            image[i][j] = (image[i][j] + image2[i][j])/2;
+        }
+    }
+}
+
+void f4_flip_h() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE/2; j++) {
+            swap(image[i][j],image[i][(SIZE-1)-j]) ;
+        }
+    }
+}
+
+void f4_flip_v(){
+    for (int i = 0; i < SIZE/2; i++){
+        for (int j = 0; j < SIZE; j++){
+            swap(image[i][j],image[(SIZE-1)-i][j]);
+        }
+    }
+}
+
+void f5_rotate() {
     for (int i = 0; i < SIZE / 2; i++) {
         for (int j = i; j < SIZE - i - 1; j++) {
             char helper = image[i][j];
             // Swap elements of each cycle
-            // in clockwise direction
+            // in clockwise direction by 90 degree
             image[i][j] = image[SIZE - 1 - j][i];
             image[SIZE - 1 - j][i] = image[SIZE - 1 - i][SIZE - 1 - j];
             image[SIZE - 1 - i][SIZE - 1 - j] = image[j][SIZE - 1 - i];
@@ -129,11 +103,37 @@ void f5_rotate() {
     }
 }
 
+void f6_Shrink() {
+
+    int  min;
+    cout<<"1-to shrink image dimension to 1/2:\n2-to shrink image dimension to 1/3:\n3-to shrink image dimension to 1/4:\n";
+    cin >> min;
+    min+=1;
+
+
+    int row = 0;
+    
+    for (int i = 0; i < SIZE; i++) {
+        int col = 0;
+        for (int j = 0; j < SIZE; j++) {
+            if (i > SIZE / min || j > SIZE / min) {
+                image[i][j] = 255;
+
+            } else
+                image[i][j] = image[row][col];
+            col += min;
+        }
+        row += min;
+    }
+}
 
 int main() {
     char choose;
     int choose1,repeat;
+
+    // main window
     while(choose!='0'){
+
         cout<<"Ahlan ya user ya habibi :)"<<endl;
         loadImage();
 
@@ -147,28 +147,41 @@ int main() {
             <<"     0- Exit\n"
             <<"     >> ";cin>>choose;
 
-
+        
         switch (choose)
         {
+            // filter 1 : black & white
             case '1':
                 f1_black_white();
                 saveImage();
                 break;
 
+            // filter 2 : invert
             case '2':
                 f2_invert();
                 saveImage();
                 break;
 
+            // filter 3 : merge
             case '3':
-                Merge();
+                f3_Merge();
                 saveImage();
                 break;
 
+            // filter 4 : flip
             case '4':
-//  agamy
+                char fl;
+                f4:
+                cout<<"\nFlip (h)orizontally or (v)ertically ?"
+                "\n>> ";cin>>fl;
+                if(fl=='h' || fl=='H'){f4_flip_h(); saveImage();}
+                
+                else if(fl=='v' || fl=='V'){f4_flip_v(); saveImage();}
+
+                else{cout<<"wrong input please try again"; goto f4;}
                 break;
 
+            // filter 5 : rotate
             case '5':
             f5:
                 cout<<"Rotate (90), (180) or (270) degrees?"<<endl
@@ -194,8 +207,9 @@ int main() {
                 else{cout<<"Wrong input please try again"; goto f5;}
                 break;
 
+            // filter 6 : shrink
             case '6':
-                Shrink();
+                f6_Shrink();
                 saveImage();
                 break;
 
@@ -203,6 +217,7 @@ int main() {
                 cout<<"Wrong inpur please try again";
                 break;
         }
+        // clear screen
         system("CLS");
     }
     return 0;
